43f052d2a3db79805295def63902acaa49fd2b65
Ting Zhou <limtingzhou@gmail.com>
2018-12-30 21:40:58 +0800

    Add route handler for all articles and specific articles
    Use chainable route handlers.

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true} ));
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
 
+
+//Creates mongoose schema with title and content of an article.
 const articleSchema = mongoose.Schema({
   title: {
     type: String,
@@ -22,10 +24,14 @@ const articleSchema = mongoose.Schema({
   }
 });
 
+//Creates mongoose model in the database.
 const Article = mongoose.model("Article", articleSchema);
 
 
-app.get("/articles", function(req, res) {
+//This is a chainable route handler for requests of all articles - /article.
+//There are 3 route handlers: get, post, delete.
+app.route("/articles")
+.get(function(req, res) { // no parameters are sent to the API. API returns all articles.
   Article.find({}, function(err, allArticles) {
     if (!err) {
       res.send(allArticles);
@@ -33,9 +39,84 @@ app.get("/articles", function(req, res) {
       res.send(err);
     }
   });
+})
+.post(function(req, res) { //title and content of article is sent to the API. API creates article object and updates database.
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+  newArticle.save(function(err) {
+    if (!err) {
+      res.send("Article successfully added.");
+    } else {
+      res.send(err);
+    }
+  });
+})
+.delete(function(req, res) { //no parameters are sent to the API. API deletes ALL article objects from the database.
+  Article.deleteMany({}, function(err) {
+    if(!err) {
+      res.send("All articles have been deleted");
+    } else {
+      res.send(err);
+    }
+  });
 });
 
 
+// This is a chainable route handler for requests of specific articles - article/<ArticleName>.
+// There are 4 route handlers - get, put, patch, delete.
+app.route("/articles/:articleName")
+.get(function(req,res) { //name of article is sent to the API. API returns article object.
+  let inputArticleName = req.params.articleName;
+  Article.findOne({title: inputArticleName}, function(err, foundArticle) {
+    if (!err) {
+      res.send(foundArticle);
+    } else {
+      res.send(err);
+    }
+  });
+})
+.put(function(req,res) { //name of article, new title AND new content of article is sent to the API. API updates article object.
+  let inputArticleName = req.params.articleName;
+  Article.update(
+    {title: inputArticleName},
+    {title: req.body.title, content: req.body.content},
+    {overwrite: true},
+    function(err) {
+      if (!err) {
+        res.send("Successfully updated article");
+      } else {
+        res.send("Could not update article");
+      }
+    }
+  );
+
+})
+.patch(function(req, res) { //name of article, new title OR new content of article is sent to the API. API only updates article object of fields sent.
+  let inputArticleName = req.params.articleName;
+  Article.update(
+    {title: inputArticleName},
+    {$set: req.body},
+    function(err) {
+      if (!err) {
+        res.send("Successfully updated article");
+      } else {
+        res.send("Could not update article");
+      }
+    }
+  );
+})
+.delete(function(req, res) { //tile of article is sent to the API. API deletes article object from database.
+  let inputArticleName = req.params.articleName;
+  Article.deleteOne({title: inputArticleName}, function(err) {
+    if (!err) {
+      res.send("Successfully deleted article");
+    } else {
+      res.send("Could not delete article");
+    }
+  });
+});
 
 
 app.listen(3000, function() {
